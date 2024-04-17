179b4cf2f4b7fc96f66a301011768bfe91287a1b
양은수 <93758339+diddmstn@users.noreply.github.com>
2024-04-17 20:27:42 +0900

    Init

---
+++ Assets/Scripts/StageButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageButton : MonoBehaviour
+{
+    public void GoMain()
+    {
+        if (CompareTag("Stage1"))
+        {
+            GameManager.sceneVariable.level = 1;
+        }
+        else if(CompareTag("Stage2"))
+        {
+            GameManager.sceneVariable.level = 1;
+        }
+        else if (CompareTag("Stage3"))
+        {
+            GameManager.sceneVariable.level = 1;
+        }
+
+        SceneManager.LoadScene("MainScene");
+    }
+  
+}
